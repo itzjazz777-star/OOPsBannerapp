982943d841a7cb73b2608ea8202a45ebc4f57e3b
itzjazz777-star <itzjazz777@gmail.com>
2026-03-14 20:43:02 +0530

    Add files via upload

---
+++ BannerUC5.java
@@ -0,0 +1,16 @@
+public class BannerUC5 {
+    public static void main(String[] args) {
+
+        String[] banner = {
+            "  OOO   OOO   PPPP   SSSS ",
+            " O   O O   O  P   P S     ",
+            " O   O O   O  PPPP   SSS  ",
+            " O   O O   O  P         S ",
+            "  OOO   OOO   P     SSSS  "
+        };
+
+        for(String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
